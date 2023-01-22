585d4722b4c8256d4de5114581bcb238d001d3ec
Miurionochek <ivanfedonuk95@gmail.com>
2023-01-22 16:01:40 +0200

    Revert "Revert "Added deleting tasklists""
    This reverts commit 784ab80b3fe3a9dcddb06a941fa7ca0621045fc4.

--- src/component/List_Item/List_Item.tsx
+++ src/component/List_Item/List_Item.tsx
@@ -6,20 +6,37 @@ import Collapse from '@mui/material/Collapse';
 import List from '@mui/material/List';
 import Menu from '../Menu/Menu';
 
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import ListOptions from './ListOptions';
+import { useSelector } from 'react-redux';
+interface listProps {
+    id: string,
+    title:string
+  }
 
 export default function ListItem(props: any) {
     const [open, setOpen] = React.useState(true);
+    const user = useSelector((state: any) => state.user);
+    const lists = useSelector((state:any)=>state.lists.lists);
+    const params = useParams();
 
     const handleClick = () => {
         setOpen(!open);
     };
+
+    let titleID:string = "";
+    lists.forEach((list:listProps) => {
+        if(list.title === params.title){
+          titleID = list.id
+        }
+      })
     return (
         <React.Fragment key={props.title}>
             <Link to={"/" + props.title}>
                 <ListItemButton>
                     <ListItemText primary={props.title} />
-                    <Menu option={'task'} />
+                    {/* <Menu option={'task'} /> */}
+                    <ListOptions uid={user.id} id={titleID} />
                 </ListItemButton>
             </Link>
         </React.Fragment>
